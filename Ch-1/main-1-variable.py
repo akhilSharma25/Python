a271dbf85d01aab0140bf2afb47ca98f6a4bddcc
AKhil Sharma <akhilkatyan25b@gmail.com>
2026-01-20 12:58:16 +0530

    variable & datatype  completed

---
+++ Ch-1/main-1-variable.py
@@ -0,0 +1,21 @@
+# print("Hello world") # single line comment
+
+"""fdkfdsbbkdvkbdskbs
+sdijdhkdndhjj """ # multi line comment or doc string
+
+#variable = it is use to store value  or variables are simply containers for storing data values
+name='Akhil Sharma'
+age=12
+
+# @age=193   # error 
+# 12hjjd=32939  #error
+#  sjd=394 # error
+
+
+sher="Lion"
+
+# Naming conventions
+# Pascal Case
+# Camal Case
+
+
---
+++ Ch-1/main-2-datatype.py
@@ -0,0 +1,24 @@
+
+#integer
+a=12
+print(type(a))
+
+#Float for real value like decimal or fraction
+b=1.2
+d=12/3
+print(d)
+print(type(d))
+
+#complex
+v=34j # j is important here to define the variable as comples number
+print(type(v))
+
+#String '' ""
+name='Ram'
+name="Ram Ram"
+print(type(name))
+
+#Boolean
+t=True
+f=False
+print(t)

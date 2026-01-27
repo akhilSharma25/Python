b8849a997b552a26d57c689f1576ba54150ab497
AKhil Sharma <akhilkatyan25b@gmail.com>
2026-01-27 11:26:08 +0530

    loop questions

--- Ch-1/main-4-String.py
+++ Ch-1/main-4-String.py
@@ -5,7 +5,7 @@ print(ord(a)) # it return unicode or ascii code
 a=65
 print(chr(a))#convert into char
 
-b="Hello Brother" 
+b="HelloBrother" 
 
 #index start from 0 positive
 #It is also start from negative but from end side
@@ -14,8 +14,8 @@ b="Hello Brother"
 #         B r o t h e r
 #        -7-6-5-4-3-2-1
 
-print(b[-1])
-print(b[-5])
+# print(b[-1])
+# print(b[-5])
 
 #slicing
 
@@ -26,5 +26,25 @@ print(b[6:9:1])
 print(b[:]) # full string
 print(b[:5])
 
+#reverse
+# m=input("Enter string")
+# print(m[::-1])
+
+# reverse
+# b=""
+# for i in range(len(m)-1,-1,-1):
+#     b+=m[i]
+
+# print(b)
+
+# print(m==b)#palindrome
+
+
+
+#String func
+
+print(b.capitalize())
+print(b.count('o'))
+print(b.isalpha())
 
 
---
+++ Ch-2/main-4-loop-questions.py
@@ -0,0 +1,86 @@
+
+#Q1
+# n=int(input("please tell your number"))
+
+# for i in range(n):
+#     print("Hello World!")
+
+
+#Q2 print natural number
+
+# n=int(input("please tell your number"))
+
+# for i in range(1,n+1):
+#     print(i)
+
+
+#Q3 reverse for loop n to 1
+
+# n=int(input("please tell your number"))
+
+# for i in range(n,0,-1):
+#     print(i)
+
+# Q4 table
+
+# n=int(input("please tell your number"))
+# for i in range(1,11):
+#     print(f"{n} * {i} = {i*n}")
+
+# Q5 sum of n term
+
+# n=int(input("please tell your number"))
+# sum=0
+# for i in range(1,n+1):
+#     sum+=i
+# print(sum)
+
+
+#Q6 Factorial number
+
+# n=int(input("please tell your number"))
+# fact=1
+# for i in range(1,n+1):
+#     fact*=i
+
+# print(fact)
+
+# Q7 sum of all even & odd number in range
+
+# n=int(input("please tell your number"))
+# evenSum=0
+# oddSum=0
+
+# for i in range(1,n+1):
+#     if(i%2==0):
+#         evenSum+=i
+#     else:
+#         oddSum+=i
+
+# print(f"Even sum is {evenSum} , Odd Sum is {oddSum}")
+
+# Q8 print all factors of number
+# n=int(input("please tell your number"))
+# for i in range(1,n+1):
+#     if(n%i==0):
+#         print(i)
+
+# Q9 prime or not
+# n=int(input("please tell your number"))
+# b=True
+# for i in range (2,n):
+#     if n%i!=0:
+#         continue
+#     else:
+#         b=False
+
+# if b:
+#     print("Prime")
+# else:
+#     print("Not prime")
+
+
+
+
+
+
